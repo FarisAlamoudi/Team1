4233176e69806dcd2d8e8facdc30ef2487bdb86f
Keating Sane <keatingsane@outlook.com>
2024-05-26 14:09:49 -0400

    Update UpdateContact.php

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -7,6 +7,7 @@
 	$newPhoneNumber = $inData["phoneNumber"];
 	$newEmailAddress = $inData["emailAddress"];
 	$updateId = $inData["updateId"];
+	$userId = $inData["userId"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error)
@@ -15,8 +16,8 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, PhoneNumber=?, EmailAddress=? WHERE ID=?");
-		$stmt->bind_param("ssssi", $newFirstName, $newLastName, $newPhoneNumber, $newEmailAddress, $updateId);
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, PhoneNumber=?, EmailAddress=? WHERE (ID=? AND UserID=?)");
+		$stmt->bind_param("ssssii", $newFirstName, $newLastName, $newPhoneNumber, $newEmailAddress, $updateId, $userId);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
