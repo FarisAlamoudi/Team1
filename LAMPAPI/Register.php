b1ec4b194df934a88c0429ec5260049b0c98f807
Keating Sane <keatingsane@outlook.com>
2024-05-25 20:47:59 -0400

    Update Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -14,7 +14,7 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+		$stmt = $conn->prepare("INSERT INTO Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
 		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
 		$stmt->execute();
 		$stmt->close();
