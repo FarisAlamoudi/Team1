c767de60040f4edf13a4e3f21ec67bd1273946d7
Keating Sane <keatingsane@outlook.com>
2024-05-27 22:44:18 -0400

    Update Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -3,9 +3,9 @@
 	$inData = getRequestInfo();
 
 	$firstName = $inData["firstName"];
-    	$lastName = $inData["lastName"];
-    	$login = $inData["login"];
-    	$password = $inData["password"];
+    $lastName = $inData["lastName"];
+    $userName = $inData["userName"];
+    $password = $inData["password"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error)
@@ -14,22 +14,24 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT * FROM Users WHERE Login=?");
-		$stmt->bind_param("s", $login);
+		$stmt = $conn->prepare("SELECT * FROM Users WHERE userName=?");
+		$stmt->bind_param("s", $userName);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
 		if ($result->num_rows > 0)
 		{
+			http_response_code(409);
 			returnWithError("Username Taken");
 		}
 		else
 		{
-			$stmt = $conn->prepare("INSERT INTO Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
-			$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+			$stmt = $conn->prepare("INSERT INTO Users (firstName,lastName,userName,password) VALUES(?,?,?,?)");
+			$stmt->bind_param("ssss", $firstName, $lastName, $userName, $password);
 			$stmt->execute();
 			$stmt->close();
 			$conn->close();
+			http_response_code(200);
 			returnWithError("");
 		}
 	}
@@ -39,13 +41,13 @@
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson($obj)
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
 
-	function returnWithError( $err )
+	function returnWithError($err)
 	{
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
