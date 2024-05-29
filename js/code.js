90597276bbf75c298be350e5d894312a7cc6e372
Keating Sane <keatingsane@outlook.com>
2024-05-28 23:27:45 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,17 +1,29 @@
 const urlBase = 'http://cosmiccontacts.net/LAMPAPI';
 const extension = 'php';
-var currentContactId; //Track Current UserID
-let userId = 0;
 
-// WORKS but no md5 password hashing
+let ID = 0;
+let USER = "";
+
+// temp solution to ID issues
+function storeID(ID)
+{
+    localStorage.setItem('storedID', ID);
+}
+function getStoredID()
+{
+    return localStorage.getItem('storedID');
+}
+
+// need better ID solution
 function login()
 {
     document.getElementById('loginResult').innerHTML = "";
+
     let userName = document.getElementById("loginName").value.trim();
     let password = document.getElementById("loginPassword").value.trim();
-    // var hash = md5(password);
-
-    let jsonPayload = JSON.stringify({userName:userName,password:password});
+    var hash = md5(password);
+    ID = 0;
+    let jsonPayload = JSON.stringify({userName:userName,password:hash});
     let url = urlBase + '/Login.' + extension;
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
@@ -22,20 +34,18 @@ function login()
         {
             if (this.status === 200)
             {
-                //userId = jsonObject.id;
                 let response = JSON.parse(xhr.responseText);
-                if (response.id > 0)
-                {
-                    window.location.href = "contacts.html";
-                }
-                else
-                {
-                    document.getElementById("loginResult").innerHTML = "Incorrect Username or Password";
-                }
+                ID = response.userId;
+                USER = response.userName;
+                storeID(ID);
+                // console.log(ID) == correct ID
+                // console.log(USER) == correct USER
+                saveCookie();
+                window.location.href = "contacts.html";
             }
             else if (this.status === 401)
             {
-                document.getElementById("loginResult").innerHTML = "No Records Found";
+                document.getElementById("loginResult").innerHTML = "Incorrect Username or Password";
             }
             else
             {
@@ -46,14 +56,57 @@ function login()
     xhr.send(jsonPayload);
 }
 
-// WORKS but no md5 password hashing and NO field validation / regex
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "ID=" + ID + ",USER=" + USER + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	ID = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if(tokens[0] == "USER")
+		{
+			USER = tokens[1];
+		}
+		else if(tokens[0] == "ID")
+		{
+			ID = parseInt(tokens[1].trim());
+		}
+	}
+
+	if(ID < 0)
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+        document.getElementById("userName").innerHTML = "USER: " + USER;
+	}
+}
+
+// needs REGEX hints (might be html job)
 function register()
 {
     document.getElementById('registerResult').innerHTML = "";
+    document.getElementById("firstNameError").innerHTML = "";
+    document.getElementById("lastNameError").innerHTML = "";
+    document.getElementById("userNameError").innerHTML = "";
+    document.getElementById("passwordError").innerHTML = "";
+
     let firstName = document.getElementById("firstName").value.trim();
     let lastName = document.getElementById("lastName").value.trim();
     let userName = document.getElementById("userName").value.trim();
     let password = document.getElementById("password").value.trim();
+    var hash = md5(password);
     var validity = true;
     
     if (firstName == "")
@@ -67,14 +120,14 @@ function register()
         validity = false;
     }
     
-    var regex = /(?=.*[a-zA-Z])([a-zA-Z0-9]).{6,20}$/;
+    var regex = /^(?=.*\d)(?=.*[a-zA-Z]).{4,16}$/;
     if (regex.test(userName) == false)
     {
         document.getElementById("userNameError").innerHTML = "Invalid User Name";
         validity = false;
     }
 
-    var regex = /(?=.*\d)(?=.*[A-Za-z])(?=.*[!@#$%^&*]).{6,22}/;
+    var regex = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).{4,16}$/;
     if (regex.test(password) == false)
         {
         document.getElementById("passwordError").innerHTML = "Invalid Password";
@@ -87,7 +140,7 @@ function register()
         return;
     }
 
-    let jsonPayload = JSON.stringify({firstName:firstName,lastName:lastName,userName:userName,password:password});
+    let jsonPayload = JSON.stringify({firstName:firstName,lastName:lastName,userName:userName,password:hash});
     let url = urlBase + '/Register.' + extension;
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
@@ -98,7 +151,6 @@ function register()
         {
             if (this.status === 200)
             {
-                //userId = jsonObject.id;
                 document.getElementById("registerResult").innerHTML = "User added";
                 window.location.href="index.html";
             }
@@ -115,59 +167,100 @@ function register()
     xhr.send(jsonPayload);
 }
 
-// top left button on contacts page returns to login screen
 function logout()
 {
-    userId = 0;
-    firstName = "";
-    lastName = "";
-
-    document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    ID = 0;
+    USER = "";
+    document.cookie = "USER= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
     window.location.href = "index.html";
 }
 
-// 
-function showTable() {
+// ???
+function showTable()
+{
     var addContactForm = document.getElementById("addContactForm");
     var contacts = document.getElementById("contacts");
 
-    if (addContactForm.style.display === "none") {
+    if (addContactForm.style.display === "none")
+    {
         addContactForm.style.display = "block";
         contacts.style.display = "none";
-    } else {
+    }
+    else
+    {
         addContactForm.style.display = "none";
         contacts.style.display = "block";
     }
 }
 
-
-// 
+// needs REGEX hints (might be html job) | need better ID solution
 function addContact()
 {
-    let firstName = document.getElementById("firstName").value;
-    let lastName = document.getElementById("lastName").value;
-    let phoneNumber = document.getElementById("phoneNumber").value;
-    let emailAddress = document.getElementById("emailAddress").value;
+    document.getElementById('addResult').innerHTML = "";
+    document.getElementById("firstNameError").innerHTML = "";
+    document.getElementById("lastNameError").innerHTML = "";
+    document.getElementById("phoneNumberError").innerHTML = "";
+    document.getElementById("emailAddressError").innerHTML = "";
 
-    let jsonPayload = JSON.stringify({firstName:firstName,lastName:lastName,phoneNumber:phoneNumber,emailAddress:emailAddress,userId:userId});
+    let firstName = document.getElementById("firstName").value.trim();
+    let lastName = document.getElementById("lastName").value.trim();
+    let phoneNumber = document.getElementById("phoneNumber").value.trim();
+    let emailAddress = document.getElementById("emailAddress").value.trim();
+    let validity = true;
+
+    document.getElementById('addResult').innerHTML = "";
+
+    if (firstName == "")
+    {
+        document.getElementById("firstNameError").innerHTML = "Invalid First Name";
+        validity = false;
+    }
+
+    if (lastName == "")
+    {
+        document.getElementById("lastNameError").innerHTML = "Invalid Last Name";
+        validity = false;
+    }
+
+    var regex = /^\d{3}-\d{3}-\d{4}$/;
+    if (regex.test(phoneNumber) == false)
+    {
+        document.getElementById("phoneNumberError").innerHTML = "Invalid Phone Number";
+        validity = false;
+    }
+
+    var regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/;
+    if (regex.test(emailAddress) == false)
+    {
+        document.getElementById("emailAddressError").innerHTML = "Invalid Email Address";
+        validity = false;
+    }
+
+    if (!validity)
+    {
+        document.getElementById("addResult").innerHTML = "INVALID";
+        return;
+    }
+
+    let jsonPayload = JSON.stringify({firstName:firstName,lastName:lastName,phoneNumber:phoneNumber,emailAddress:emailAddress,userId:getStoredID()});
     let url = urlBase + '/AddContact.' + extension;
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     xhr.onreadystatechange = function()
     {
-        if (this.readyState == 4 && this.status == 200)
+        if (this.readyState == 4)
         {
-            document.getElementById("registerResult").innerHTML = "Contact added";
-            document.getElementById("addMe").reset();
-            loadContacts();
+            document.getElementById("addResult").innerHTML = "Contact added";
             showTable();
         }
     };
     xhr.send(jsonPayload);
 }
 
-function showAddForm() {
+// ???
+function showAddForm()
+{
     //Get the addContact Form
     var form = document.getElementById("addContactForm");
 
@@ -175,37 +268,37 @@ function showAddForm() {
     form.style.display = "block";
 }
 
-function cancelAdd() {
+// ???
+function cancelAdd()
+{
     // Get the form element
     var form = document.getElementById("addContactForm");
 
     // Set the display property to "none" to hide the form
     form.style.display = "none";
-
-    // Clear the input fields
-    document.getElementById("firstName").value = "";
-    document.getElementById("lastName").value = "";
-    document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
 }
 
-//
-function loadContacts() {
-    let jsonPayload = JSON.stringify({ search: "", userId: userId });
+// ???
+function searchContacts()
+{
+    let jsonPayload = JSON.stringify({search:"",userId:getStoredID()});
     let url = urlBase + '/SearchContacts.' + extension;
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+    xhr.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
             let response = JSON.parse(xhr.responseText);
             let text = "<table border='1'><thead><tr><th>First Name</th><th>Last Name</th><th>Email Address</th><th>Phone Number</th><th></th></tr></thead><tbody>";
-            for (let i = 0; i < response.results.length; i++) {
+            for (let i = 0; i < response.results.length; i++)
+            {
                 text += "<tr id='row" + i + "'>";
-                text += "<td id='first_Name" + i + "'>" + response.results[i].FirstName + "</td>";
-                text += "<td id='last_Name" + i + "'>" + response.results[i].LastName + "</td>";
-                text += "<td id='email" + i + "'>" + response.results[i].EmailAddress + "</td>";
-                text += "<td id='phone" + i + "'>" + response.results[i].PhoneNumber + "</td>";
+                text += "<td id='first_Name" + i + "'>" + response.results[i].firstName + "</td>";
+                text += "<td id='last_Name" + i + "'>" + response.results[i].lastName + "</td>";
+                text += "<td id='email" + i + "'>" + response.results[i].emailAddress + "</td>";
+                text += "<td id='phone" + i + "'>" + response.results[i].phoneNumber + "</td>";
                 text += "<td>" +
                     "<button type='button' id='edit_button" + i + "' class='w3-button w3-circle w3-lime' onclick='editContact(" + i + ")'>Edit</button>" +
                     "<button type='button' id='save_button" + i + "' value='Save' class='w3-button w3-circle w3-lime' onclick='updateContact(" + i + ")' style='display: none'>Save</button>" +
@@ -219,8 +312,9 @@ function loadContacts() {
     xhr.send(jsonPayload);
 }
 
-
-function editContact(contactId) {
+// ???
+function editContact(contactId)
+{
     // Store the id of the contact being edited
     currentContactId = contactId;
 
@@ -240,7 +334,9 @@ function editContact(contactId) {
     document.getElementById("editContactForm").style.display = "block";
 }
 
-function updateContact() {
+// ???
+function updateContact()
+{
     // Get the new contact details from the form
     var firstName = document.getElementById("editFirstName").value;
     var lastName = document.getElementById("editLastName").value;
@@ -256,9 +352,9 @@ function updateContact() {
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
     try {
         xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
+            if (this.readyState == 4 && this.status == 200)
+            {
                 console.log("Contact has been updated");
-                loadContacts();
             }
         };
         xhr.send(jsonPayload);
@@ -270,71 +366,7 @@ function updateContact() {
     document.getElementById("editContactForm").style.display = "none";
 }
 
-/* Replaced by editContact
-function edit_row(id) {
-    document.getElementById("edit_button" + id).style.display = "none";
-    document.getElementById("save_button" + id).style.display = "inline-block";
-
-    var firstNameI = document.getElementById("first_Name" + id);
-    var lastNameI = document.getElementById("last_Name" + id);
-    var email = document.getElementById("email" + id);
-    var phone = document.getElementById("phone" + id);
-
-    var namef_data = firstNameI.innerText;
-    var namel_data = lastNameI.innerText;
-    var email_data = email.innerText;
-    var phone_data = phone.innerText;
-
-    firstNameI.innerHTML = "<input type='text' id='namef_text" + id + "' value='" + namef_data + "'>";
-    lastNameI.innerHTML = "<input type='text' id='namel_text" + id + "' value='" + namel_data + "'>";
-    email.innerHTML = "<input type='text' id='email_text" + id + "' value='" + email_data + "'>";
-    phone.innerHTML = "<input type='text' id='phone_text" + id + "' value='" + phone_data + "'>"
-}*/
-
-/* Replaced by updateContact
-function save_row(no) {
-    var namef_val = document.getElementById("namef_text" + no).value;
-    var namel_val = document.getElementById("namel_text" + no).value;
-    var email_val = document.getElementById("email_text" + no).value;
-    var phone_val = document.getElementById("phone_text" + no).value;
-    var id_val = ids[no]
-
-    document.getElementById("first_Name" + no).innerHTML = namef_val;
-    document.getElementById("last_Name" + no).innerHTML = namel_val;
-    document.getElementById("email" + no).innerHTML = email_val;
-    document.getElementById("phone" + no).innerHTML = phone_val;
-
-    document.getElementById("edit_button" + no).style.display = "inline-block";
-    document.getElementById("save_button" + no).style.display = "none";
-
-    let tmp = {
-        phoneNumber: phone_val,
-        emailAddress: email_val,
-        newFirstName: namef_val,
-        newLastName: namel_val,
-        id: id_val
-    };
-
-    let jsonPayload = JSON.stringify(tmp);
-
-    let url = urlBase + '/UpdateContact.' + extension;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try {
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                console.log("Contact has been updated");
-                loadContacts();
-            }
-        };
-        xhr.send(jsonPayload);
-    } catch (err) {
-        console.log(err.message);
-    }
-}*/
-
+// ???
 function deleteContact(no) 
 {
     var namef_val = document.getElementById("first_Name" + no).innerText;
@@ -346,7 +378,7 @@ function deleteContact(no)
         {
         document.getElementById("row" + no + "").outerHTML = "";
 
-        let jsonPayload = JSON.stringify({firstName:firstName,lastName:lastName,userId:userId});
+        let jsonPayload = JSON.stringify({firstName:firstName,lastName:lastName,userId:getStoredID()});
         let url = urlBase + '/DeleteContact.' + extension;
         let xhr = new XMLHttpRequest();
         xhr.open("POST", url, true);
@@ -359,63 +391,4 @@ function deleteContact(no)
             };
             xhr.send(jsonPayload);
     };
-}
-
-function searchContacts() {
-    const content = document.getElementById("searchText");
-    const selections = content.value.toUpperCase().split(' ');
-    const table = document.getElementById("contacts");
-    const tr = table.getElementsByTagName("tr");// Table Row
-
-    for (let i = 0; i < tr.length; i++) {
-        const td_fn = tr[i].getElementsByTagName("td")[0];// Table Data: First Name
-        const td_ln = tr[i].getElementsByTagName("td")[1];// Table Data: Last Name
-
-        if (td_fn && td_ln) {
-            const txtValue_fn = td_fn.textContent || td_fn.innerText;
-            const txtValue_ln = td_ln.textContent || td_ln.innerText;
-            tr[i].style.display = "none";
-
-            for (selection of selections) {
-                if (txtValue_fn.toUpperCase().indexOf(selection) > -1) {
-                    tr[i].style.display = "";
-                }
-                if (txtValue_ln.toUpperCase().indexOf(selection) > -1) {
-                    tr[i].style.display = "";
-                }
-            }
-        }
-    }
-    // Show the table after search
-    $('.tables').show();
-}
-
-
-function contactIsValid(firstName, lastName, phoneNumber, emailAddress) {
-
-    var validity = true;
-
-    if (firstName == "") {
-        document.getElementById("contactResult").innerHTML = "Invalid First Name";
-        validity = false;
-    }
-
-    if (lastName == "") {
-        document.getElementById("contactResult").innerHTML = "Invalid Last Name";
-        validity = false;
-    }
-
-    var regex = /^[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/;
-    if (regex.test(phoneNumber) == false) {
-        document.getElementById("contactResult").innerHTML = "Invalid Phone Number";
-        validity = false;
-    }
-
-    var regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-    if (regex.test(emailAddress) == false) {
-        document.getElementById("contactResult").innerHTML = "Invalid Email Address";
-        validity = false;
-    }
-
-    return validity;
 }
