b2c4db944d0fea6c82f731bb337670b154e5eddc
Keating Sane <keatingsane@outlook.com>
2024-05-28 21:17:59 -0400

    Update SearchContacts.php

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -1,13 +1,4 @@
 <?php
-	header("Access-Control-Allow-Origin: http://www.cosmiccontacts.net");
-	header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-	header("Access-Control-Allow-Headers: Content-Type, Authorization");
-	
-	// Handle preflight requests
-	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-		http_response_code(200);
-		exit();
-	}
 
 	$inData = getRequestInfo();
 	
